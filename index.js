706c2d81be1b7d527bdecea24b4395b0e8fc367e
Mashuke Alam Jim <jimmashuke@gmail.com>
2021-07-31 12:31:54 +0600

    Routes Setup [Base Route]

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
 const express = require('express');
 
+// app
 const app = express();
+
+// routes
+const baseRouter = require('./routes/base');
 
 // app configs
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-const PORT = process.env.NODE_PORT || 3333;
 
-app.get('/', (req, res) => {
-    res.send('ki obostha?');
-});
+app.use('/', baseRouter);
 
 // redirecting every other requests as error
 app.use((req, res) => {
     res.status(404).send('lost in the jungle of binary bits? 404_ERROR_NOT_FOUND')
 });
 
+
+// Listen @ designated port
+const PORT = process.env.NODE_PORT || 3333;
 app.listen(PORT, () => {
     console.log(`Alive @ localhost:${PORT}`);
 });

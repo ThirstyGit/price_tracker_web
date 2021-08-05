64b77c62cceea48e093f7f128d9228af6788aa7e
James Zaman <jzaman182040@bscse.uiu.ac.bd>
2021-08-05 11:32:31 +0600

    Saving user type. If no type is set default to user. Also added some comments.

--- routes/auth.js
+++ routes/auth.js
@@ -6,15 +6,25 @@ const bcrypt = require("bcrypt");
 const { User } = require('../database/database.js');
 
 router.post("/register", async (req, res) => {
+  // Making sure the email is not already registered.
   const user = await User.findOne({ email: req.body.email });
   if (user) {
     res.json({ message: "User Already Exists!" });
-  } else {
+  }
+  else {
+    // Checking if user type is already set. If not, default to 'user'
+    type = 'user'
+    if(req.body.type) {
+      type = req.body.type;
+    }
+    // Hashing the password before sotring it to the database.
     req.body.password = bcrypt.hashSync(req.body.password, 14);
+    // Creating the data and saving it to the database.
     const user = new User({
       name: req.body.name,
       email: req.body.email,
       password: req.body.password,
+      type
     });
     user.save();
     res.json({ message: "success" });

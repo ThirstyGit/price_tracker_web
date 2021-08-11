8762b2543f9598161d2fed71d76f2007911091ce
James Zaman <jzaman182040@bscse.uiu.ac.bd>
2021-08-11 23:19:08 +0600

    Scrap function now extracts name and price based on give parameters.

--- functions/scrap.js
+++ functions/scrap.js
@@ -1,9 +1,22 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
 
-async function scrap(url, parmas) {
+async function scrap(url, params, cb) {
+  // Requesting the page and loading it to cheerio.
   const page = await axios.get(url);
-  return 'Function not complete.';
+  const $ = cheerio.load(page.data);
+
+  // Extracting the names.
+  $(params.name).each((index, data) => {
+    console.log($(data).text().replace(/\s\s+/g, ""));
+  });
+  // Extracting the prices.
+  $(params.price).each((index, data) => {
+    console.log($(data).text().replace(/\s\s+/g, ''));
+  });
+  
+  // Running any callback if passed.
+  if(cb) cb();
 };
 
 module.exports = scrap;

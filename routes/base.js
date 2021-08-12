1bbe8ea658fb3c61cbfcf3e2eb900b841461bcab
safat khan <mkhan182039@bscse.uiu.ac.bd>
2021-08-12 14:12:49 +0600

    set the homepage path

--- routes/base.js
+++ routes/base.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.status(200).send("Came from the wild...");
-});
+});*/
+router.get('/', (req, res) => {
+  res.render('homepage');
+})
 
 module.exports = router;

c67a5983393acda35e7848753be1c1eb17ee169c
Mashuke Alam Jim <jimmashuke@gmail.com>
2021-08-13 21:59:43 +0600

    Product Page with Third Party Graph (ChartJS)

--- routes/tracking.js
+++ routes/tracking.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 const { Products } = require('../database/database');
 /* GET home page. */
-router.get('/:id', async (req, res, next) => {
-    const id = req.params.id;
+router.get('/', async (req, res, next) => {
+
     // console.log(id);
-    let currProducts = await Products.find(  );
+    let currProducts = await Products.find();
     // console.log(currProducts);
     let refinedArray = [];
     currProducts.forEach(el => {
@@ -20,15 +20,26 @@ router.get('/:id', async (req, res, next) => {
             } else if (lastTwo[0] < lastTwo[1]) {
                 label = ["⬇️", "Going down"]
             }
-            refinedArray.push({el, label, lastTwo});
-            
+            refinedArray.push({ el, label, lastTwo });
+
         } else {
             lastTwo = [el.price_history[lenPrice - 1].price, el.price_history[lenPrice - 1].price];
-            refinedArray.push({el, label, lastTwo});
+            refinedArray.push({ el, label, lastTwo });
         }
     });
-    console.log(refinedArray[0]);
-    res.render('tracking', {products: refinedArray});
+    // console.log(refinedArray[0]);
+    res.render('tracking', { products: refinedArray });
+});
+
+router.get('/prod/:id', async (req, res) => {
+    console.log(1234);
+    const ID = req.params.id;
+    const desiredProduct = await Products.find({_id: ID});
+    console.log(desiredProduct[0]);
+    const prices = desiredProduct[0].price_history.map(({price}) => parseFloat(price.replace(/[^\d.-]/g, '')));
+    const times = desiredProduct[0].price_history.map(({timestamp}) => new Date(`${timestamp}`).toLocaleTimeString()); // Bloody hell timestamp!!
+    console.log(prices, times);
+    res.render('product', {title: desiredProduct[0].name, link: desiredProduct[0].link, prices, times});
 });
 
 module.exports = router;

e76d7418c45bec7859f3e48658f064f0f77c83c2
safat khan <mkhan182039@bscse.uiu.ac.bd>
2021-08-15 11:56:48 +0600

    set the user page path

--- routes/base.js
+++ routes/base.js
@@ -8,5 +8,7 @@ var router = express.Router();
 router.get('/', (req, res) => {
   res.render('homepage');
 })
-
+router.get('/user', (req, res) => {
+  res.render('user');
+})
 module.exports = router;

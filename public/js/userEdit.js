d8a17ae4b64d50106e7b4f20828de3ef15f2dc50
safat khan <mkhan182039@bscse.uiu.ac.bd>
2021-09-17 16:44:55 +0600

    fetch function for userupdate

--- public/js/userEdit.js
+++ public/js/userEdit.js
@@ -2,6 +2,8 @@ const userForm = document.querySelector('#user-form');
 const passwordForm = document.querySelector('#password-form');
 const name = document.querySelector('#name');
 const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const passwordConfirm = document.querySelector('#confirm-password');
 const changeInfo = document.querySelector('#change-info');
 const changePassword = document.querySelector('#change-password');
 const infoChangeContainer = document.querySelector('#info-change-container');
@@ -19,7 +21,51 @@ changeInfo.addEventListener('click', event => {
     passwordChangeContainer.classList.remove('hidden');
  });
 
+
+ /* ----------------FETCH Function ------------------------- */ 
  userForm.addEventListener('submit', event=>{
+    event.preventDefault();
+    if(!email.value.match(/(^.+@.+\.com$)/)){
+            alert("Give a valid email");
+    }
+    else{
+       fetch('/userEditInfo',{
+          method:"PUT",
+          body:JSON.stringify({
+             name:name.value,
+             email:email.value
+          }),
+          headers:{
+            'Content-type': 'application/json; charset=UTF-8'
+          }
+       })
+       .catch(err => {
+         console.error(err);
+       })
+    }
 
  });
  
+ passwordForm.addEventListener('submit', event => {
+   event.preventDefault();
+   if(!password.value.match(/(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])(.{8})/)) {
+      alert('Password must contain Uppercase Lowercase and at least 8 characters');
+   }
+   else if(password.value != passwordConfirm.value) {
+      alert("Password fields don't match")
+   }
+   else {
+      fetch('/changepassword', {
+         method: "PUT",
+         body: JSON.stringify({
+            password: password.value
+         }),
+         headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+         }
+      })
+      .catch(err => {
+         console.error(err);
+       })
+   }
+})
--- routes/base.js
+++ routes/base.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require("bcrypt");
+// Importing database modules.
+const { User } = require('../database/database.js');
 
 /* GET home page. */
 /*router.get('/', function(req, res, next) {
@@ -8,14 +11,31 @@ var router = express.Router();
 router.get('/', (req, res) => {
   res.render("homepage", { user: req.user });
 })
+
 router.get('/user', (req, res) => {
   res.render('user', { user: req.user });
 })
+
 router.get('/userRequest', (req, res) => {
   res.render('userRequest');
 })
+
 router.get('/userEdit', (req, res) => {
   res.render('userEdit',{ user: req.user });
+})
+
+
+
+//********************************** */
+
+router.put('/userEditInfo', async (req, res) => {
+  const user= await User.findOneAndUpdate(
+    {_id:req.user._id},
+    { $set: { name:req.body.name,email:req.body.email}}   
+  );
+  user.save();
+  //console.log(req.user);
+  res.send("");
 })
 
 

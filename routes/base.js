ae9544de9426bed8961825e58c8cb3d3d5c294d9
safat khan <mkhan182039@bscse.uiu.ac.bd>
2021-09-23 08:27:39 +0600

    products read from producttable and showed in homepage

--- routes/base.js
+++ routes/base.js
@@ -2,24 +2,28 @@ var express = require('express');
 var router = express.Router();
 const bcrypt = require("bcrypt");
 // Importing database modules.
-const { User } = require('../database/database.js');
+const { User, Products } = require('../database/database.js');
 
 /* GET home page. */
 /*router.get('/', function(req, res, next) {
   res.status(200).send("Came from the wild...");
 });*/
 router.get('/', async (req, res) => {
-
-  res.render("homepage", { user: req.user});
+  const products = await Products.find().limit(9);
+  //console.log(products);
+  res.render("homepage", { user: req.user, products});
 })
+
 
 router.get('/user', (req, res) => {
   res.render('user', { user: req.user });
 })
 
+
 router.get('/userRequest', (req, res) => {
   res.render('userRequest');
 })
+
 
 router.get('/userEdit', (req, res) => {
   res.render('userEdit',{ user: req.user });

6835f431064b9c6e65f4a63463bb590cc48fc402
Mashuke Alam Jim <jimmashuke@gmail.com>
2021-09-24 20:30:39 +0600

    open and close function for popup added

---
+++ public/js/product.js
@@ -0,0 +1,12 @@
+const popupDiv = document.querySelector('.popup');
+const overlayDiv = document.getElementById('overlay');
+const openpop = () => {
+    popupDiv.classList.add('active');
+    overlayDiv.classList.add('active');
+}
+
+const closepop = () => {
+    popupDiv.classList.remove('active');
+    overlayDiv.classList.remove('active');
+}
+

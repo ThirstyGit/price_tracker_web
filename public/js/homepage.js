887ee2983658387d7f4127ed7ef21d348a8f81c1
safat khan <mkhan182039@bscse.uiu.ac.bd>
2021-09-24 14:39:00 +0600

    improved search suggestion style

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -5,23 +5,14 @@ const dropdown = document.querySelector('#dropdown');
 
 // Open or close dropdown when clicked on hamburger.
 dropdownHamburger.addEventListener("click", () => {
-    if(window.getComputedStyle(dropdown).opacity === '0') {
-       dropdown.style.opacity = 1; 
-       dropdown.style.transform = 'translateY(0px)';
-       dropdown.style.pointerEvents = 'all';
-    }
-    else {
-       dropdown.style.opacity = 0;
-       dropdown.style.transform = 'translateY(-20px)';
-       dropdown.style.pointerEvents = 'none';
-    }
- })
-/*
- // search with enter.
-searchBar.addEventListener('keypress', (e) => {
-   if(e.keyCode === 13) {
-      // getting the domain name out.
-      const hostName = window.location.href.split('/')[2];
-      window.location.href = `http://${hostName}/search/${searchBar.value}`;
+   if(window.getComputedStyle(dropdown).opacity === '0') {
+      dropdown.style.opacity = 1; 
+      dropdown.style.transform = 'translateY(0px)';
+      dropdown.style.pointerEvents = 'all';
    }
-})*/
+   else {
+      dropdown.style.opacity = 0;
+      dropdown.style.transform = 'translateY(-20px)';
+      dropdown.style.pointerEvents = 'none';
+   }
+ })
